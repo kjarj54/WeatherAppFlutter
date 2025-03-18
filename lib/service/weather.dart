ebe65e5e91bf7442abe8383cb4de1de8f7d4a1cb
Arauz <kjarj54@gmail.com>
2025-03-17 19:55:01 -0600

    [UPDATE ]

--- lib/service/weather.dart
+++ lib/service/weather.dart
@@ -1,36 +1,33 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future<Map<String, dynamic>> getWeather(String longitude, String latitude) async {
-  
-
-  String url =
-      'https://api.openweathermap.org/data/3.0/onecall?lat=$latitude&lon=$longitude&appid=69386c6cb777decb20f24d475d2d51ad&lang=es&units=metric';
+Future<Map<String, dynamic>> getWeather(String latitude, String longitude) async {
+  final String apiKey = '69386c6cb777decb20f24d475d2d51ad';
+  final String url = 'https://api.openweathermap.org/data/3.0/onecall?lat=$latitude&lon=$longitude&appid=$apiKey&lang=es&units=metric';
 
   try {
-    var response = await http.get(Uri.parse(url));
+    final response = await http.get(Uri.parse(url));
+    
     if (response.statusCode == 200) {
-      var result = jsonDecode(response.body);
-      print("Respuesta: ${response.body}");
+      final Map<String, dynamic> data = jsonDecode(response.body);
       return {
         'current': {
-          'temp': result['current']['temp'],
-          'description': result['current']['weather'][0]['description'],
-          'humidity': result['current']['humidity'],
-          'clouds': result['current']['clouds'],
+          'temp': data['current']['temp'],
+          'description': data['current']['weather'][0]['description'],
+          'humidity': data['current']['humidity'],
+          'clouds': data['current']['clouds'],
         },
         'daily': List.generate(3, (index) => {
-          'temp_max': result['daily'][index]['temp']['max'],
-          'temp_min': result['daily'][index]['temp']['min'],
-          'description': result['daily'][index]['weather'][0]['description'],
-          'humidity': result['daily'][index]['humidity'],
+          'temp_max': data['daily'][index]['temp']['max'],
+          'temp_min': data['daily'][index]['temp']['min'],
+          'description': data['daily'][index]['weather'][0]['description'],
+          'humidity': data['daily'][index]['humidity'],
         }),
       };
     } else {
-      throw Exception('Fallo cargano el clima');
+      throw Exception('Error: ${response.statusCode}');
     }
   } catch (e) {
-    print(e);
-    throw Exception( e.toString());
+    throw Exception('Error: $e');
   }
 }
